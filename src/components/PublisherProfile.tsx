1934877475e7d9128d56040432c1c52ec0b16b9f
kris guru <krisbs1122@gmail.com>
2024-05-17 18:34:06 +0530

    view more label change

--- src/components/PublisherProfile.tsx
+++ src/components/PublisherProfile.tsx
@@ -8,7 +8,7 @@ const PublisherProfile: React.FC<{
         <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png" alt="" className='rounded w-8'/>
         {/* <strong className='text-sm'>Builder Profile</strong> */}
         <strong className='text-base'>{title}</strong>
-        <button className='text-xs p-0 px-2 rounded'>Follow</button>
+        <button className='text-xs p-0 px-2 rounded'>View More</button>
     </>
   )
 }
