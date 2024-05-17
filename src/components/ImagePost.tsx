6c9c4423ed36a92d06bb3f93579a98314a1db766
kris guru <krisbs1122@gmail.com>
2024-05-17 18:54:14 +0530

    Image post component added

---
+++ src/components/ImagePost.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ContactActions from './ContactActions'
+import PublisherProfile from './PublisherProfile'
+
+const ImagePost: React.FC<{
+    title: string,
+    description: string,
+    src: string
+}> = ({title, description, src}) => {
+    return <div className='mt-5'>
+        <div className='flex gap-2 items-center'>
+            <PublisherProfile title={title} />
+        </div>
+        <img src={src} alt="" className='w-full h-full'/>
+        <div>
+        <ContactActions getQuote={true} />
+        <p className='text-xl mt-2 lg:text-base '><strong>{title}</strong> {description}</p>
+        <p className='relative  text-base opacity-80'>2 hours ago</p>
+        </div>
+    </div>
+}
+
+export default ImagePost
