6d6fb3638e6b51cf16933addea34aa8fe3ee2616
kris guru <krisbs1122@gmail.com>
2024-05-18 01:47:38 +0530

    swipeable carousel gallery post

--- src/components/CarouselSlider.tsx
+++ src/components/CarouselSlider.tsx
@@ -1,47 +1,35 @@
 import React, { useState } from 'react';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
+import {Navigation, Pagination, Scrollbar, A11y} from 'swiper/modules';
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const CarouselSlider: React.FC<{
   slides: { id: number; image: string; alt: string }[];
 }> = ({slides}) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
-  };
-
-  const nextSlide = () => {
-    setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
-  };
 
   return (
     <div className="relative">
-      <div className="carousel">
-        <div className="carousel-inner relative overflow-hidden w-full">
-          {slides.map((slide, index) => (
-            <div
-              key={slide.id}
-              className={`carousel-item float-left w-full ${
-                index === currentSlide ? 'block' : 'hidden'
-              }`}
-            >
+      <Swiper modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {
+          slides.map((slide, index) => (
+            <SwiperSlide key={slide.id}>
               <img src={slide.image} className="w-full" alt={slide.alt} />
-            </div>
-          ))}
-        </div>
-      </div>
-      <button
-        className="carousel-control-prev absolute top-1/2 left-0 transform -translate-y-1/2 bg-black-100 text-white p-1 rounded-r"
-        onClick={prevSlide}
-      >
-        <FaAngleLeft />
-      </button>
-      <button
-        className="carousel-control-next absolute top-1/2 right-0 transform -translate-y-1/2 bg-black-100 text-white p-1 rounded-l"
-        onClick={nextSlide}
-      >
-        <FaAngleRight />
-      </button>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
     </div>
   );
 };
