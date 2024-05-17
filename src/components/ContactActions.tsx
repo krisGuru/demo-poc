017f43235442749f3edb40f0c7c8e263c5a00b2f
kris guru <krisbs1122@gmail.com>
2024-05-17 17:37:27 +0530

    contack action component changes

--- src/components/ContactActions.tsx
+++ src/components/ContactActions.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { FaPhone, FaWhatsapp, FaShareSquare } from 'react-icons/fa'
 
-const ContactActions = () => {
+const ContactActions: React.FC<{
+  getQuote: boolean
+}> = ({
+  getQuote=false
+}) => {
   return (
     <>
-      <FaPhone className='text-2xl fa-phone' />
-      <FaWhatsapp className='text-2xl'/>
-      <FaShareSquare className='text-2xl' />
+    {
+      getQuote && <>
+        <a href="#" className='float-right text-sm mr-4 border border-gray-600 px-2 rounded'>
+          Get Quote
+        </a>
+      </>
+    }
+      <div style={{display:"flex", gap: "1rem", marginTop: "1rem", marginLeft: "1rem"}}>
+        <FaPhone className='text-2xl fa-phone' />
+        <FaWhatsapp className='text-2xl'/>
+        <FaShareSquare className='text-2xl' />
+      </div>
     </>
   )
 }
