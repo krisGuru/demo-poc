2ee8572ffaaa3c342bb679c7c2d4bfaca7018403
kris guru <krisbs1122@gmail.com>
2024-05-24 14:36:05 +0530

    unnecessary command line removal

--- src/components/PublisherProfile.tsx
+++ src/components/PublisherProfile.tsx
@@ -6,7 +6,6 @@ const PublisherProfile: React.FC<{
   return (
     <>
         <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png" alt="" className='rounded w-8'/>
-        {/* <strong className='text-sm'>Builder Profile</strong> */}
         <strong className='text-base'>{title}</strong>
         <button className='text-xs p-0 px-2 rounded'>View More</button>
     </>
