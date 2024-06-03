c3499615a40d59c144fc6f8f85ee548f9d00bad5
kris guru <krisbs1122@gmail.com>
2024-06-03 10:46:56 +0530

    profile page path determined

--- src/components/PublisherProfile.tsx
+++ src/components/PublisherProfile.tsx
@@ -5,13 +5,15 @@ const PublisherProfile: React.FC<{
 }> = ({title}) => {
   return (
     <>
-    <div className='flex items-center gap-3'>
+    <div className='flex items-center gap-3'
+    onClick={e=>{window.location.href='/profile'}}>
       <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png" alt="" className='rounded w-8'/>
       <strong className='text-base truncate'>{title}</strong>
-      <button className='text-xs p-0 px-2 rounded w-24'>View More</button>
+      <button  className='text-xs p-0 px-2 rounded w-24'>View More</button>
     </div>
     </>
   )
 }
 
 export default PublisherProfile;
+  
