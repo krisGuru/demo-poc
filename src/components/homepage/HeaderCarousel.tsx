9de63e4b616cae0c13bbe2fc3fa7237ab0d252e3
kris guru <krisbs1122@gmail.com>
2024-06-03 13:56:54 +0530

    header carousal changes

--- src/components/homepage/HeaderCarousel.tsx
+++ src/components/homepage/HeaderCarousel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {  useState } from 'react';
 
 import {Navigation, Pagination, Scrollbar, A11y} from 'swiper/modules';
 import {Swiper, SwiperSlide} from 'swiper/react';
@@ -23,29 +23,91 @@ const HeaderCarousel = () => {
         { src: 'https://foyr.com/learn/wp-content/uploads/2021/10/rules-for-interior-designers-1024x656.png', alt: 'Image 3', title: 'Ceiling Properties' },
         { src: 'https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg', alt: 'Image 4', title: 'Table Properties' },
     ]
-  return (
-    <Swiper className="image-carousel" modules={[Navigation, Pagination, Scrollbar, A11y]}
-    spaceBetween={50}
-    slidesPerView={3}
-    scrollbar={{ draggable: true }}
-    onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}
-    >
-        {
-            images.map((image, index) => (
-                <SwiperSlide key={index}>
-                  <div className='images-card'>
-                      <div>
-                          <img key={index} src={image.src} alt={image.alt} />
-                      </div>
-                      <div>
-                          <span>{image.title}</span>
-                      </div>
-                  </div>
-                </SwiperSlide>
-            ))
+
+    const subImages = [
+      { src: 'https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg', alt: 'Image 1', title: 'Glass Door' },
+      { src: 'https://media.designcafe.com/wp-content/uploads/2023/07/05195443/modern-interior-design.jpg', alt: 'Image 2', title: 'Wooden Door' },
+      { src: 'https://foyr.com/learn/wp-content/uploads/2021/10/rules-for-interior-designers-1024x656.png', alt: 'Image 3', title: 'Plastic Door' },
+      { src: 'https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg', alt: 'Image 4', title: 'Net Door' },
+      { src: 'https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg', alt: 'Image 1', title: 'Fiber Door' },
+      { src: 'https://media.designcafe.com/wp-content/uploads/2023/07/05195443/modern-interior-design.jpg', alt: 'Image 2', title: 'Auto lock doors' },
+    ]
+
+    const [showSubCategory, setShowSubCategory] = useState(false)
+    const [categorySlideCount, setCategorySlideCount] = useState(3)
+
+
+    const handleClick = (index: number) => {
+        setShowSubCategory(true)
+        document.querySelector
+        let images = document.getElementsByClassName('category-images');
+        for(let i=0; i<images.length; i++){
+            images[i].classList.add('mini')
         }
-    </Swiper>
+        setCategorySlideCount(6)
+    };
+
+    const revertBack = (index: number) => {
+      setShowSubCategory(false)
+      let images = document.getElementsByClassName('category-images');
+      for(let i=0; i<images.length; i++){
+          images[i].classList.remove('mini')
+      }
+      setCategorySlideCount(3)
+    }
+
+  return (
+    <>
+      <Swiper
+        className="image-carousel mini"
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={20}
+        slidesPerView={categorySlideCount}
+        scrollbar={{ draggable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index} onClick={() => handleClick(index)}>
+            <div className='images-card category-images px-2'>
+              <div>
+                <img src={image.src} alt={image.alt} className='w-5' />
+              </div>
+              <div className='text-guide'>
+                <span className='text-sm'>{image.title}</span>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+        {
+            showSubCategory && <>
+        <Swiper 
+        className="image-carousel"
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={20}
+        slidesPerView={3}
+        scrollbar={{ draggable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {subImages.map((image, index) => (
+          <SwiperSlide key={index} onClick={() => revertBack(index)}>
+              <div className='images-card px-2'>
+                <div>
+                  <img src={image.src} alt={image.alt} />
+                </div>
+                <div>
+                  <span className='text-sm'>{image.title}</span>
+                </div>
+              </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+            </>
+        }
+      
+    </>
   );
 };
 
