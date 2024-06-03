ba61ceac5e3a41c8da4034103b9f0b880b1b5d46
kris guru <krisbs1122@gmail.com>
2024-06-03 20:20:41 +0530

    images config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+nextConfig.images = {
+  domains: ['media.designcafe.com', 'foyr.com', "cdn.iconscout.com",'www.trade4asia.com'],
+};
+
+
+
 export default nextConfig;
