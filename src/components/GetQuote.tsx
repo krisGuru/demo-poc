d3752c26ff917e566f2a88968cef00c67e2af5b4
kris guru <krisbs1122@gmail.com>
2024-06-04 13:09:26 +0530

    size refactor

--- src/components/GetQuote.tsx
+++ src/components/GetQuote.tsx
@@ -5,9 +5,9 @@ const GetQuote = () => {
   return (
   <>
   <Link href={'/getquoteform'}>
-    <div className='bg-blue-950 text-white w-full p-1'>
+    <div className='bg-blue-950 text-white w-full p-2'>
         <div className="text-right">
-          <h1 className='text-xs mr-2'>Get Quote &gt;</h1>
+          <h1 className='text-sm mr-2'>Get Quote &gt;</h1>
         </div>
     </div>
   </Link>
