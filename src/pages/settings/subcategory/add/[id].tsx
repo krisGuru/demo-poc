046b6c48c43506e0cb72094d0f4f3800bdd9e4a8
kris guru <krisbs1122@gmail.com>
2024-06-12 16:39:44 +0530

    add subcategory moved to new folder

--- src/pages/settings/subcategory/add/[id].tsx
+++ src/pages/settings/subcategory/add/[id].tsx
@@ -1,21 +1,33 @@
-import React, { useState } from 'react'
-import '../../../app/globals.css'
+import React, { useEffect, useState } from 'react'
+import '@/app/globals.css'
 import SideNav from '@/components/SideNav'
 import Link from 'next/link'
 import Head from 'next/head'
 import { IoChevronBackCircleOutline } from 'react-icons/io5'
 import apiCall from '@/utils/apiCall'
+import { useRouter } from 'next/router'
 
 const AddSubCategory = () => {
+    const router = useRouter();
+    const {id} = router.query;
+
     const [subCategoryForm, setSubCategoryForm] = useState({
         subcategory_name: '',
         subcategory_logo: null,
         subcategory_description: ''
     })
+    const [categoryID, setCategoryID] = useState<string>('')
+
+    useEffect(()=>{
+        if(id)
+            setCategoryID(id.toString())
+    },[id])
+
     const handleSubmit = async()=>{
         const fd = new FormData();
         fd.append('subcategory_name', subCategoryForm.subcategory_name)
         fd.append('subcategory_description', subCategoryForm.subcategory_description)
+        fd.append('category_id', categoryID)
         const subcategory_logo = document.getElementById('subcategory-logo') as HTMLInputElement
         if( subcategory_logo.files && subcategory_logo.files?.length > 0)
             fd.append('subcategory_logo', subcategory_logo.files[0])
@@ -36,7 +48,7 @@ const AddSubCategory = () => {
             </Head>
             <div id="video-post-container" className="py-5">
                 <span className='settings-profile'>
-                    <Link href={'/settings/subcategory'}>
+                    <Link href={'/settings/subcategory/'+id}>
                         <div className='settings-back'>
                             <IoChevronBackCircleOutline className='mt-[1px]'/> 
                             <span>Back</span>
@@ -75,8 +87,8 @@ const AddSubCategory = () => {
                         </div>
                         <div className='save-button'>
                             <button className='save-btn'
-                            onClick={handleSubmit}
-                            >Save</button>
+                            onClick={e=>{window.location.href='/settings/subcategory/add/'+id}}
+                           >Save</button>
                         </div>
                     </div>
                 </div>
